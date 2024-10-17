bdde875412dc4747fb8f40551fa1127f9e7015c8
Saha-Shibani <sahashibani57@gmail.com>
2024-10-17 21:49:34 +0530

    database and env config added

---
+++ server/db/auto-margin.go
@@ -0,0 +1,10 @@
+package db
+
+import (
+	"github.com/shiibs/ticket-booking-project-v1/models"
+	"gorm.io/gorm"
+)
+
+func DBMigrator(db *gorm.DB) error {
+	return db.AutoMigrate(&models.Event{})
+}
