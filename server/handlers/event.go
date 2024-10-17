e8872f592629a2f9e50b6a355635611dee2e1e4b
Saha-Shibani <sahashibani57@gmail.com>
2024-10-17 21:15:17 +0530

    basic setup for backend done

---
+++ server/handlers/event.go
@@ -0,0 +1,71 @@
+package handlers
+
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/shiibs/ticket-booking-project-v1/models"
+)
+
+type EventHandler struct {
+	repository models.EventRepository
+}
+
+func (h *EventHandler) GetMany(ctx *fiber.Ctx) error {
+	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	defer cancel()
+
+	events, err := h.repository.GetMany(context)
+
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+			"status":  "fail",
+			"message": err.Error(),
+		})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(&fiber.Map{
+		"status":  "success",
+		"message": "",
+		"data":    events,
+	})
+}
+
+func (h *EventHandler) GetOne(ctx *fiber.Ctx) error {
+	eventId, _ := strconv.Atoi(ctx.Params("eventId"))
+
+	context, cancel := context.WithTimeout(context.Background(), time.Duration(5*time.Second))
+	defer cancel()
+
+	event, err := h.repository.GetOne(context, uint(eventId))
+
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+			"status":  "fail",
+			"message": err.Error(),
+		})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(&fiber.Map{
+		"status":  "success",
+		"message": "",
+		"data":    event,
+	})
+}
+
+func (h *EventHandler) CreateOne(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func NewEventHandler(router fiber.Router, repository models.EventRepository) {
+	handler := &EventHandler{
+		repository: repository,
+	}
+
+	router.Get("/", handler.GetMany)
+	router.Post("/", handler.CreateOne)
+	router.Get("/:eventId", handler.GetOne)
+
+}
---
+++ server/repositories/event.go
@@ -0,0 +1,29 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/shiibs/ticket-booking-project-v1/models"
+)
+
+type EventRepository struct {
+	db any
+}
+
+func (r *EventRepository) GetMany(ctx context.Context) ([]*models.Event, error) {
+	return nil, nil
+}
+
+func (r *EventRepository) GetOne(ctx context.Context, eventId uint) (*models.Event, error) {
+	return nil, nil
+}
+
+func (r *EventRepository) CreateOne(ctx context.Context, event models.Event) (*models.Event, error) {
+	return nil, nil
+}
+
+func NewEventRepository(db any) models.EventRepository {
+	return &EventRepository{
+		db: db,
+	}
+}
