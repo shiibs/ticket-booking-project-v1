7c380105a061a15e924a2161f174d776542152f9
Saha-Shibani <sahashibani57@gmail.com>
2024-10-21 15:00:06 +0530

    user register and login added

--- server/main.go
+++ server/main.go
@@ -7,7 +7,9 @@ import (
 	"github.com/shiibs/ticket-booking-project-v1/config"
 	"github.com/shiibs/ticket-booking-project-v1/db"
 	"github.com/shiibs/ticket-booking-project-v1/handlers"
+	"github.com/shiibs/ticket-booking-project-v1/middlewares"
 	"github.com/shiibs/ticket-booking-project-v1/repositories"
+	"github.com/shiibs/ticket-booking-project-v1/services"
 )
 
 func main() {
@@ -23,9 +25,15 @@ func main() {
 
 	eventRepository := repositories.NewEventRepository(db)
 	ticketRepository := repositories.NewTicketRepository(db)
+	authRepository := repositories.NewAuthRepository(db)
 
-	handlers.NewEventHandler(server.Group("/event"), eventRepository)
-	handlers.NewTicketHandler(server.Group("/ticket"), ticketRepository)
+	authService := services.NewAuthService(authRepository)
+	handlers.NewAuthHandler(server.Group("/auth"), authService)
+
+	privateRoutes := server.Use(middlewares.AuthProtected(db))
+
+	handlers.NewEventHandler(privateRoutes.Group("/event"), eventRepository)
+	handlers.NewTicketHandler(privateRoutes.Group("/ticket"), ticketRepository)
 
 	app.Listen(fmt.Sprintf(":" + envConfig.ServerPort))
 
