ab56925b67fc1093b0eed051a9f4dcbcbc866a03
Saha-Shibani <sahashibani57@gmail.com>
2024-10-24 17:16:31 +0530

    updated logout and ticketlist screen

--- client/app/(authed)/(tabs)/settings.tsx
+++ client/app/(authed)/(tabs)/settings.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/Button";
+import { VStack } from "@/components/VStack";
+import { useAuth } from "@/context/AuthContext";
+
 export default function SettingScreen() {
-  return <></>;
+  const { logout } = useAuth();
+
+  return (
+    <VStack flex={1} m={20}>
+      <Button onPress={logout}>Logout</Button>
+    </VStack>
+  );
 }
